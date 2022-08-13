d5d6f3b5500636a718818709628dc3c5c8bb417c
Fatih20 <fnri39@protonmail.com>
2022-08-13 16:24:32 +0700

    Initialize page structure.

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,69 +7,70 @@ import Header from "../components/Header";
 import SearchBar from "../components/SearchBar";
 import TagName from "../components/TagName";
 import InstructorApplication from "../components/InstructorApplication";
+import BaseLayout from "../layout/BaseLayout";
 
 const Home: NextPage = () => {
   return (
-    <div className="flex justify-center items-start flex-col w-full">
-      <Header />
-      {/* <SearchBar /> */}
-      <TagName
-        tagName="Bruhflkvmvmck"
-        runOnClick={() => {
-          return;
-        }}
-      />
-      <CourseForStudent
-        title="Test"
-        thumbnail="https://www.manorhousestables.co.uk/wp-content/uploads/2015/03/placeholder_image_1000.jpg"
-        tag="lkvmfv"
-        instructorName="lkvfkdlv"
-        shortenedDescription="l;fmdklvmfdklkcmsdfdsmlkvdmskkmlmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmlvmsdklvdsmklvmkll"
-      />
-      <CourseForInstructor
-        title="Test"
-        thumbnail="https://www.manorhousestables.co.uk/wp-content/uploads/2015/03/placeholder_image_1000.jpg"
-        shortenedDescription="l;fmdklvmfdklkcmsdfdsmlkvdmskkmlmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmlvmsdklvdsmklvmkll"
-        status="verified"
-        peopleEnrolled={56}
-      />
-      <CourseForAdmin
-        title="Test"
-        thumbnail="https://www.manorhousestables.co.uk/wp-content/uploads/2015/03/placeholder_image_1000.jpg"
-        shortenedDescription="l;fmdklvmfdklkcmsdfdsmlkvdmskkmlmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmlvmsdklvdsmklvmkll"
-        instructor="lkmvkd"
-        selected={true}
-        runOnApprove={() => {
-          return;
-        }}
-        runOnReject={() => {
-          return;
-        }}
-        runOnDeselect={() => {
-          return;
-        }}
-        runOnSelect={() => {
-          return;
-        }}
-      />
-      <InstructorApplication
-        username="Test"
-        email="https://www.manorhousestables.co.uk/wp-content/uploads/2015/03/placeholder_image_1000.jpg"
-        selected={true}
-        runOnApprove={() => {
-          return;
-        }}
-        runOnReject={() => {
-          return;
-        }}
-        runOnDeselect={() => {
-          return;
-        }}
-        runOnSelect={() => {
-          return;
-        }}
-      />
-    </div>
+    <BaseLayout showBackButton={false}>
+      <div className="flex justify-start items-center flex-col w-full">
+        <TagName
+          tagName="Bruhflkvmvmck"
+          runOnClick={() => {
+            return;
+          }}
+        />
+        <CourseForStudent
+          title="Test"
+          thumbnail="https://www.manorhousestables.co.uk/wp-content/uploads/2015/03/placeholder_image_1000.jpg"
+          tag="lkvmfv"
+          instructorName="lkvfkdlv"
+          description="l;fmdklvmfdklkcmsdfdsmlkvdmskkmlmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmlvmsdklvdsmklvmkll"
+        />
+        <CourseForInstructor
+          title="Test"
+          thumbnail="https://www.manorhousestables.co.uk/wp-content/uploads/2015/03/placeholder_image_1000.jpg"
+          description="l;fmdklvmfdklkcmsdfdsmlkvdmskkmlmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmlvmsdklvdsmklvmkll"
+          status="verified"
+          peopleEnrolled={56}
+        />
+        <CourseForAdmin
+          title="Test"
+          thumbnail="https://www.manorhousestables.co.uk/wp-content/uploads/2015/03/placeholder_image_1000.jpg"
+          description="l;fmdklvmfdklkcmsdfdsmlkvdmskkmlmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmlvmsdklvdsmklvmkll"
+          instructor="lkmvkd"
+          selected={true}
+          runOnApprove={() => {
+            return;
+          }}
+          runOnReject={() => {
+            return;
+          }}
+          runOnDeselect={() => {
+            return;
+          }}
+          runOnSelect={() => {
+            return;
+          }}
+        />
+        <InstructorApplication
+          username="Test"
+          email="https://www.manorhousestables.co.uk/wp-content/uploads/2015/03/placeholder_image_1000.jpg"
+          selected={true}
+          runOnApprove={() => {
+            return;
+          }}
+          runOnReject={() => {
+            return;
+          }}
+          runOnDeselect={() => {
+            return;
+          }}
+          runOnSelect={() => {
+            return;
+          }}
+        />
+      </div>
+    </BaseLayout>
   );
 };
 
