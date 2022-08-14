4104e003f0efd627071d8c8521d18a508d4884c0
Fatih20 <fnri39@protonmail.com>
2022-08-14 14:58:48 +0700

    Fix bug on teacher selection.

--- components/auth/instructorOrStudentButton.tsx
+++ components/auth/instructorOrStudentButton.tsx
@@ -11,16 +11,6 @@ export default function InstructorOrStudentButton({
     setRole(value);
   };
 
-  // Manage Green Check Visibility
-  let instructorCheckClass = "absolute";
-  let studentrCheckClass = "absolute";
-
-  if (role != "INSTRUCTOR") {
-    instructorCheckClass = "absolute hidden";
-  } else {
-    studentrCheckClass = "absolute hidden";
-  }
-
   return (
     <div className="flex space-x-4">
       <div className="relative basis-1/2 w-full">
@@ -31,7 +21,7 @@ export default function InstructorOrStudentButton({
           <img src="https://static.vecteezy.com/system/resources/previews/003/241/285/original/business-instructor-and-tutor-vector.jpg" />
           <h1>Instructor</h1>
           <img
-            className={instructorCheckClass}
+            className={`absolute ${role === "TEACHER" ? "" : "hidden"}`}
             style={{ bottom: "-7px", right: "-6px", height: 40 }}
             src="https://cdn-icons-png.flaticon.com/512/190/190411.png"
           />
@@ -45,7 +35,7 @@ export default function InstructorOrStudentButton({
           <img src="https://cdn.pixabay.com/photo/2019/05/21/22/21/boy-4220282__340.png" />
           <h1>Student</h1>
           <img
-            className={studentrCheckClass}
+            className={`absolute ${role === "STUDENT" ? "" : "hidden"}`}
             style={{ bottom: "-7px", right: "-6px", height: 40 }}
             src="https://cdn-icons-png.flaticon.com/512/190/190411.png"
           />
