1686e02a25ea38ea483b108f9ec8a416e6f87414
Satria L <satrialaksana12@gmail.com>
2022-08-15 18:02:41 +0700

    fix file download button tailwind ver problem

--- components/Lecture/fileDownloadButton.tsx
+++ components/Lecture/fileDownloadButton.tsx
@@ -4,24 +4,33 @@ export default function FileDownloadButton({ url }: { url: string }) {
 
 
 
-            <div className="flex flex-row">
-                <div className="w-3/4">
-                    <span> The_file_name.pdf</span>
+            {/* PDF Download Button Start */}
+            <div className="flex p-3 rounded-lg shadow-lg bg-white w-full">
+                <div className="w-10/12 flex items-center ">
+                    <div className="flex items-center  space-x-1 w-100">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="2em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24">
+                            <g fill="none" stroke="currentColor" strokeLinejoin="round" strokeWidth={2}>
+                                <path strokeLinecap="round" d="M4 4v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8.342a2 2 0 0 0-.602-1.43l-4.44-4.342A2 2 0 0 0 13.56 2H6a2 2 0 0 0-2 2Zm5 9h6m-6 4h3" />
+                                <path d="M14 2v4a2 2 0 0 0 2 2h4" />
+                            </g>
+                        </svg>
+                        <span>This is the pdf file name.pdf</span>
+                    </div>
                 </div>
-
-                <div className="w-1/4">
-                    <a href={url} target="_blank">
-                        <button type="button" className="w-100 bg-blue-500 hover:bg-[#3b5998]/90 focus:ring-4 focus:outline-none focus:ring-[#3b5998]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#3b5998]/55 mr-2 mb-2">
-                            <svg xmlns="http://www.w3.org/2000/svg" width={16} height={16} fill="currentColor" className="bi bi-file-text" viewBox="0 0 16 16">
-                                <path d="M5 4a.5.5 0 0 0 0 1h6a.5.5 0 0 0 0-1H5zm-.5 2.5A.5.5 0 0 1 5 6h6a.5.5 0 0 1 0 1H5a.5.5 0 0 1-.5-.5zM5 8a.5.5 0 0 0 0 1h6a.5.5 0 0 0 0-1H5zm0 2a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1H5z" />
-                                <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2zm10-1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1z" />
-                            </svg>
-                        </button>
-                    </a>
-                </div>
-
-
+                <button className="w-2/12 flex items-center justify-center  bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                    <div className="flex space-x-2 items-center justify-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 16 16">
+                            <g fill="currentColor">
+                                <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z" />
+                                <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z" />
+                            </g>
+                        </svg>
+                        <span className="hidden sm:block"> Download</span>
+                    </div>
+                </button>
             </div>
+            {/* PDF Download Button End */}
+
 
 
         </>
--- pages/componentList.tsx
+++ pages/componentList.tsx
@@ -6,24 +6,25 @@ export default function componentListPage() {
 
 
     return (
-        <div className="min-h-full flex items-center justify-center bg-blue-300 py-12 sm:px-6 lg:px-8">
-            <div className="w-full space-y-3">
+        <div className="container mx-auto w-70">
+            <div className="min-h-full flex items-center justify-center bg-blue-300 py-12 px-6 lg:px-8">
+                <div className="w-full space-y-3">
 
-                <div className="max-w-sm rounded overflow-hidden shadow-lg p-3 bg-white">
-                    <YoutubeEmbed embedId="EWfHJI3M0Fs" />
-                </div>
+                    {/* <div className="rounded overflow-hidden shadow-lg p-3 bg-white">
+                        <YoutubeEmbed embedId="EWfHJI3M0Fs" />
+                    </div> */}
 
-                <div className="max-w-sm rounded overflow-hidden shadow-lg p-3 bg-white">
-                    <ImageLecture imgUrl="https://www.looper.com/img/gallery/the-untold-truth-of-detective-conan/l-intro-1655141676.jpg" />
-                </div>
-                <div className="max-w-sm rounded overflow-hidden shadow-lg p-3 bg-white">
-
+                    <div className="rounded overflow-hidden shadow-lg p-3 bg-white">
+                        <ImageLecture imgUrl="https://www.looper.com/img/gallery/the-untold-truth-of-detective-conan/l-intro-1655141676.jpg" />
+                    </div>
+                    
                     <FileDownloadButton url="https://www.adobe.com/be_en/active-use/pdf/Alice_in_Wonderland.pdf" />
+                    
+
 
                 </div>
-
-
             </div>
+
         </div>
     )
 }
