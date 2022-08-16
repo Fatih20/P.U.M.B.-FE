84a1bcd3e805c8aaf7a71a3bdb259a08e9980e4f
Satria L <satrialaksana12@gmail.com>
2022-08-16 23:01:32 +0700

    generate youtube url form on click

--- components/Lecture/addDropDownButton.tsx
+++ components/Lecture/addDropDownButton.tsx
@@ -1,14 +1,13 @@
 import { useState } from "react";
 
-export default function AddDropDownButton() {
+export default function AddDropDownButton({handleTrigger}:{handleTrigger:any}) {
     const [showDropdown, setShowDropdown] = useState(false);
-
 
     return (
         <>
 
             <div className="relative" onClick={() => setShowDropdown(!showDropdown)}>
-                <button className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
+                <button className="w-full sm:w-auto text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
                     <span className="w-11/12 text-left">
                         + Add Item.. 
                     </span>
@@ -16,7 +15,7 @@ export default function AddDropDownButton() {
                 </button>
                 {/* Dropdown menu */}
                 {showDropdown && 
-                    <div className="w-full z-10 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 block" style={{ position: 'absolute', inset: '-26px auto auto -292px', margin: 0, transform: 'translate(291px, 70px)' }}>
+                    <div className="w-full sm:w-auto z-10 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 block" style={{ position: 'absolute', inset: '-26px auto auto -292px', margin: 0, transform: 'translate(291px, 70px)' }}>
                         <ul className="py-1 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefault">
                             <li>
                                 <a href="#" className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
@@ -29,7 +28,7 @@ export default function AddDropDownButton() {
                                 </a>
                             </li>
                             <li>
-                                <a href="#" className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
+                                <a onClick={() => handleTrigger("youtube")} className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
                                     Youtube Video Link
                                 </a>
                             </li>
--- pages/course/[courseId]/lecture/[lectureId].tsx
+++ pages/course/[courseId]/lecture/[lectureId].tsx
@@ -1,15 +1,33 @@
+import { useState } from "react"
+import { useRouter } from "next/router"
 import SecondBaseLayout from "../../../../layout/SecondBaseLayout"
 import LectureTitleForm from "../../../../components/Lecture/lectureTitle"
+import AddDropDownButton from "../../../../components/Lecture/addDropDownButton"
+import LectureItemFactory from "../../../../components/Lecture/lectureItemFactory"
 
-export default function LecturePage() {        
+
+export default function LecturePage() {
+    // Initiate Router
+    const router = useRouter()
+    const { courseId } = router.query
+    
+    const [lectureItemFormTrigger, setLectureItemFormTrigger] = useState({ generate: false, type: "" })
+
+    function handleLectureItemFormTrigger(formType: string) {
+        setLectureItemFormTrigger({ ...lectureItemFormTrigger, generate: true, type: formType })
+    }
+
+
+
     return (
         <>
             <SecondBaseLayout showBackButton={true}>
-                {/* <h1>Halo, saya uzumaki naruto</h1> */}
                 <div className="space-y-3 w-full ">
-                    <LectureTitleForm  />
-                    {/* <YoutubeEmbed embedId="EWfHJI3M0Fs" /> */}
+                    <LectureTitleForm />
 
+                    {lectureItemFormTrigger.generate && <LectureItemFactory/>}
+
+                    <AddDropDownButton handleTrigger={handleLectureItemFormTrigger} />
                 </div>
 
             </SecondBaseLayout>
