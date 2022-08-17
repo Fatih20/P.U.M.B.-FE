e866bce2937dc0167b7e69f0a84ef4e5b79ce258
Fatih20 <fnri39@protonmail.com>
2022-08-17 14:10:14 +0700

    Safeguard create course with use me.

--- pages/courses/create.tsx
+++ pages/courses/create.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import useMe from "../../hooks/useMe";
 import BaseLayout from "../../layout/BaseLayout";
 
 type Props = {};
 
 const CreateCoursePage = (props: Props) => {
+  const { user } = useMe();
   return <BaseLayout></BaseLayout>;
 };
 
